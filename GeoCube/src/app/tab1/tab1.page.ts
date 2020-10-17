796b5e892dfbb5d642be991b4fe6f2b209019caa
jordy <jordy.dj.jj@gmail.com>
2020-10-17 20:57:43 +0200

    Added an interactive map on the home screen

--- GeoCube/src/app/tab1/tab1.page.ts
+++ GeoCube/src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component , ViewChild, ElementRef} from '@angular/core';
+import { disableDebugTools } from '@angular/platform-browser';
+
+declare var google: any
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,23 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  map: any;
+@ViewChild('map', {read: ElementRef, static: false}) mapRef:ElementRef;
+  constructor() {
+
+  }
+  ionViewDidEnter(){
+    this.showMap();
+  }
+
+  showMap(){
+    const location  = new google.maps.LatLng(-17.824858, 31.053028);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  }
 
 }
